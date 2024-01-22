a8ac4d327e296c47fc0e8d312eba4085667166ae
Jeff Siarto <jsiarto@gmail.com>
2024-01-22 14:29:40 +0000

    CSS passthrough

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,5 @@
+// The export statement makes these settings available to other files in 11ty
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("images");
+  eleventyConfig.addPassthroughCopy("styles");
 };
